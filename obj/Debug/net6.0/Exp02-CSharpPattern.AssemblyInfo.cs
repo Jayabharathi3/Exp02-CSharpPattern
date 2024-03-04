1e1facfa86570c4321dbf57a43a66ed000ea5872
Jayabharathi3 <bharathibhuvana10@gmail.com>
2024-03-04 23:27:09 +0530

    success

--- obj/Debug/net6.0/Exp02-CSharpPattern.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exp02-CSharpPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exp02-CSharpPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f887ea3d0d7b752b303e8a68b661d10c6a823085")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exp02-CSharpPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exp02-CSharpPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
